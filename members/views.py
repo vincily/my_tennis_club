77096e5fea9d8d74d452387ba0818da484ed9684
vincily <vincyj6@gmail.com>
2024-09-06 19:50:11 +0530

    first commit

---
+++ members/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def members(request):
+    # template = loader.get_template('myfrist.html')
+    # return HttpResponse(template.render())
+    return HttpResponse('thosjdfslf')
